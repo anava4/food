2b2d9587fbab50eb7ac10c8eb2e5b1fd37d84e1e
anava4 <anava4@mail.greenriver.edu>
2019-04-15 16:31:03 -0400

    Added index page

--- index.php
+++ index.php
@@ -17,11 +17,18 @@ $f3->set('DEBUG',3);
 //Define a default route
 $f3->route('GET /', function()
 {
-    echo "Food!!!";
+
     //Display a view
-    //$view = new Template();
-    //echo $view->render('views/home2.html');
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
+
+//Define a breakfast route
+$f3->route('GET /breakfast', function()
+{
+    echo"<h1>Breakfast Page</h1>";
+}
+);
 
 
 //Run fat free
